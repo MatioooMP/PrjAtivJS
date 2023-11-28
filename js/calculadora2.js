3ea60414a800029c08a5e6d227977ecde0b43906
MATHEUS MORAES PEREIRA <matheus.mpereira17@senacsp.edu.br>
2023-11-27 22:12:24 -0300

    arrumando erro

--- js/calculadora2.js
+++ js/calculadora2.js
@@ -1,25 +1,37 @@
-const num1 = document.formCalculadora.num1.value;
-const num2 = document.formCalculadora.num2.value;
+/*console.log(document.querySelector('#numero1').value);
+
+console.log(document.getElementById('numero1').value);
+
+console.log(document.getElementsByTagName('input')[0].value);
+
+console.log(document.getElementsByClassName('form-input')[0].value);*/
+
+function Calcular(){
+const primeironumero = parseFloat(document.formCalculadora.num1.value);
+const segundonumero = parseFloat(document.formCalculadora.num2.value);
 const operacao = document.formCalculadora.oper.value;
 let resultado;
 
 if (operacao == '+') {
-    resultado = num1 + num2;
+    resultado = primeironumero + segundonumero;
 }
 
 else if (operacao == '-') {
-    resultado = num1 - num2;
+    resultado = primeironumero - segundonumero;
 }
 
 else if(operacao == 'x'){
-    resultado = num1*num2;
+    resultado = primeironumero*segundonumero;
 }
 
 else if(operacao == '/'){
-    resultado = num1/num2;
+    resultado = primeironumero/segundonumero;
 }
 
 
-alert('O resultado da operação é ', resultado);
+alert('O resultado da operação é: '+resultado);
 
-document.log('O resultado da operação é ')
+document.log('O resultado da operação é: ', resultado);
+
+document.write('O resultado da operação é:', resultado);
+}
